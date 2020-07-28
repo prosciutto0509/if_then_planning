efe4abcebdf5513b324e88cfd87f7c54fde3bd94
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-07-28 17:38:59 +0900

    create_idea

---
+++ app/assets/javascripts/conversation.js
@@ -0,0 +1,13 @@
+$("#samurai").on({
+  'mouseenter': function() {
+
+    $("#balloon1_p").text("こんにちは！jQueryです！");
+
+  },
+  'mouseleave': function() {
+
+    $("#balloon1_p").text("jquaryが外れました");
+
+  }
+})
+
