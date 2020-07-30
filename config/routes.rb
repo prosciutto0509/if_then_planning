73667fb23a1bb0e80290b1ced0571c76d0b0dfbf
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-07-30 22:23:05 +0900

    sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
-  root 'top#index'
+
+
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'top#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   resources :user, only: [:edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :list, only: [:new, :create, :edit, :update, :destroy] do
   resources :card, except: :index
+
 end
 end
