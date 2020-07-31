74f391f4018d9badbdaccf2e1a3d911f4dee75ab
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-07-31 10:45:09 +0900

    e

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'appname.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://ifthenplanning.herokuapp.com/' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
