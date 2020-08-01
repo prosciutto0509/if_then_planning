993acc872d5e7241ceb7c5260a71545ecb0a790a
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-08-01 14:43:57 +0900

    q

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,7 @@
 class TopController < ApplicationController
   def index
-  @lists = List.where(user: current_user).order("lists.created_at ASC")
+  @lists = List.where(user: current_user)
+
   @user  = User.where(user: current_user)
 
   end
