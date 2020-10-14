111ce89a72f351d138a22112e08c1ba0732ec886
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-10-14 12:23:58 +0900

    edit_view

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,14 +4,25 @@ class TopController < ApplicationController
   @lists = List.where(user: current_user)
   
 @user = current_user
-if @user.login_date.blank? || @user.total_login.blank?
+if @user.total_login.blank? 
+  @user.total_login = 1
+end
+
+if @user.login_date.blank? | @user.continous_login.blank?
 @user.login_date = Date.today
-@user.total_login = 1
-elsif @user.login_date + 1 == Date.today
+@user.continous_login = 1
+end
+
+if @user.login_date == Date.today 
+
+elsif @user.login_date +1 == Date.today
 @user.total_login += 1
+@user.continous_login += 1
 @user.login_date = Date.today
 else
-  @user.login_date = Date.today
+  @user.total_login += 1
+@user.continous_login = 1
+@user.login_date = Date.today
  
 end
 @user.save
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_020821) do
+ActiveRecord::Schema.define(version: 2020_10_13_235802) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_10_12_020821) do
     t.string "name", default: "", null: false
     t.date "login_date"
     t.integer "total_login"
+    t.integer "continous_login"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
